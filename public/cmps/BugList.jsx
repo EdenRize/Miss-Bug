55161c9337262cb19f509ea16d0c8b7ee3f1ebdb
EdenRize <edenrize@gmail.com>
2023-12-20 17:58:19 +0200

    Prevent non-creator from removing/editing a bug

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -1,21 +1,34 @@
 const { Link } = ReactRouterDOM
 
 import { BugPreview } from './BugPreview.jsx'
+import { userService } from '../services/user.service.js'
 
 export function BugList({ bugs, onRemoveBug }) {
+  const user = userService.getLoggedinUser()
+
+  function isCreator(bug) {
+    if (!user) return false
+    if (!bug.creator) return true
+    return user.isAdmin || bug.creator._id === user._id
+  }
+
   return (
     <ul className="bug-list clean-list">
       {bugs.map((bug) => (
         <li key={bug._id}>
           <BugPreview bug={bug} />
           <section>
-            <button onClick={() => onRemoveBug(bug._id)}>Remove</button>
             <button>
               <Link to={`/bug/${bug._id}`}>Details</Link>
             </button>
-            <button>
-              <Link to={`/bug/edit/${bug._id}`}>Edit</Link>
-            </button>
+            {isCreator(bug) && (
+              <div>
+                <button onClick={() => onRemoveBug(bug._id)}>Remove</button>
+                <button>
+                  <Link to={`/bug/edit/${bug._id}`}>Edit</Link>
+                </button>
+              </div>
+            )}
           </section>
         </li>
       ))}
